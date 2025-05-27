0c678384112e3893ca1ecc39ebd520ec16f5a5c7
MiguelAnFel <143433398+MiguelAnFel@users.noreply.github.com>
2025-05-27 11:28:56 -0300

    sim

--- script.js
+++ script.js
@@ -12,8 +12,9 @@ document.addEventListener('DOMContentLoaded', function() {
 
     const aumentaFonteBotao = document.getElementById('aumentar-fonte');
     const diminuiFonteBotao = document.getElementById('diminuir-fonte');
+    const alteraContraste = document.getElementById('alterna-contraste'):
 
-    let tamanhoAtualFonte = 1
+    let tamanhoAtualFonte = 1;
 
     aumentaFonteBotao.addEventListener('click',function(){
         tamanhoAtualFonte += 0.1;
@@ -22,5 +23,8 @@ document.addEventListener('DOMContentLoaded', function() {
     diminuiFonteBotao.addEventListener('click',function(){
         tamanhoAtualFonte -= 0.1;
         document.body.style.fontSize = `${tamanhoAtualFonte}rem`;
+    })
+    alteraContraste.addEventListener('click', function(){
+        document.body.classList.toggle('alto-contraste');
     })
 })
